40e934e9f587321f9d31ca24322398f9b61770ad
daiane-dosanjos <dayanjos01@gmail.com>
2023-06-20 17:09:30 -0300

    api

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import { toast } from "react-toastify";
 
-const API = "https://burger-queen-api-mock-tau.vercel.app";
+const API = "https://localhost:8080";
 //-------------POST-------------------------
 
 //REGISTRO DE COLABORADOR
