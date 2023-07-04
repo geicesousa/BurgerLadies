b902d52f54ea82244cd36e5c3c5619c0477d37fa
Geice Sousa Pinho <geicesousa@outlook.com>
2023-07-04 18:00:58 -0300

    push do projeto finalizado

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,7 @@
 import { toast } from "react-toastify";
 
 const API = "http://localhost:8080";
+// const API = "https://burger-queen-api-mock-tau.vercel.app"
 //-------------POST-------------------------
 
 //REGISTRO DE COLABORADOR
